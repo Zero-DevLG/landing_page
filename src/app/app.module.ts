f15205b837250eec397300cd7012271e91625ba7
DEV_LG <lgverdiguelc@gmail.com>
2024-07-21 18:50:11 -0600

    base 21/07/2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ServicesSectionComponent } from './components/services-section/services-section.component';
+import { TopBarComponent } from './components/top-bar/top-bar/top-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ServicesSectionComponent,
+    TopBarComponent,
+
   ],
   imports: [
     BrowserModule,
